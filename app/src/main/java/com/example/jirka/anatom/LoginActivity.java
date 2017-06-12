69fc72bdb95652c2998f6ac33a7d551f7dfb74b1
Jiří Papoušek <jiri.pap@gmail.com>
2017-06-12 18:47:08 +0200

    Add getting ID token from Google sign in.

--- app/src/main/java/com/example/jirka/anatom/LoginActivity.java
+++ app/src/main/java/com/example/jirka/anatom/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.jirka.anatom;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,12 @@ import android.widget.EditText;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONException;
@@ -26,6 +34,7 @@ public class LoginActivity extends FragmentActivity {
     Button signInButton;
     EditText userNameEditText;
     EditText passwordEditText;
+    SignInButton signInWithGoogleButton;
 
     private HTTPService service;
     int status;
@@ -34,6 +43,7 @@ public class LoginActivity extends FragmentActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+    GoogleApiClient mGoogleApiClient;
 
 
     @Override
@@ -44,8 +54,33 @@ public class LoginActivity extends FragmentActivity {
         signInButton = (Button) findViewById(R.id.signInButton);
         userNameEditText = (EditText) findViewById(R.id.userNameEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        signInWithGoogleButton = (SignInButton) findViewById(R.id.signInWithGoogleButton);
 
         service = new HTTPService();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestServerAuthCode(getString(R.string.server_client_id))
+                .requestIdToken(getString(R.string.server_client_id))
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInWithGoogleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, 10);
+            }
+        });
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,9 +88,6 @@ public class LoginActivity extends FragmentActivity {
                 login();
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void login() {
@@ -126,6 +158,33 @@ public class LoginActivity extends FragmentActivity {
         }
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 10) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInWithGoogleResult(result);
+        }
+    }
+
+    private void handleSignInWithGoogleResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            userNameEditText.setText(acct.getServerAuthCode());
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(LoginActivity.this).create();
+            alertDialog.setTitle("Something unexpected happened.");
+            alertDialog.setMessage("Please try again.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+    }
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
